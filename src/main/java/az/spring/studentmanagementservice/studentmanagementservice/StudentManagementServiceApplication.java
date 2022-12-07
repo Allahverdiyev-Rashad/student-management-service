ebef97e094bf3a9e85bde306f7d3be68a5ea65a0
Orkhan93 <dokta228@mail.ru>
2022-12-07 09:47:37 +0400

    initial commit

---
+++ src/main/java/az/spring/studentmanagementservice/studentmanagementservice/StudentManagementServiceApplication.java
@@ -0,0 +1,11 @@
+package az.spring.studentmanagementservice.studentmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementServiceApplication.class, args);
+    }
+}
